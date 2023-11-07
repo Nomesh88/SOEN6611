64c57c53a889bdebe972c9f50a3a846f8aee39eb
SanVinoth Pacham <45181379+sanvinoth@users.noreply.github.com>
2023-11-06 22:08:16 -0500

    Update Metricstics.py

--- Metricstics.py
+++ Metricstics.py
@@ -1,32 +1,61 @@
 import tkinter as tk
 import random
 import math
-from tkinter import ttk
+from tkinter import ttk, scrolledtext
 from tkinter.messagebox import showerror
 
-class Metricstics:
-    generated_data = None
-
-    @staticmethod
-    def abs(num):
-        if num < 0:
-            return -num
-        return num
-
-    @staticmethod
-    def generate_data(f):
+# DataGenerator class is responsible for generating random data.
+class DataGenerator:
+    def generate_data(self, f):
         try:
-            range_value = f
-            if range_value >= 1000:
-                Metricstics.generated_data = [random.randint(0, 1000) for _ in range(f)]
-                return Metricstics.generated_data
+            if f >= 1000:
+                return [random.randint(0, 1000) for _ in range(f)]
             else:
-                return ("Range must be at least 1000.")
+                return "Range must be at least 1000."
         except ValueError:
-            return ("Invalid input. Please enter a number.")
-    
-    @staticmethod
-    def mode(data):
+            return "Invalid input. Please enter a number."
+
+# Metricstics class handles various statistical calculations without using built-in functions.
+class Metricstics:
+    def calculate_min(self, data):
+        if not data:
+            return 0
+        min_value = data[0]
+        for num in data:
+            if num < min_value:
+                min_value = num
+        return min_value
+
+    def calculate_max(self, data):
+        if not data:
+            return 0
+        max_value = data[0]
+        for num in data:
+            if num > max_value:
+                max_value = num
+        return max_value
+
+    def calculate_mean(self, data):
+        if not data:
+            return 0
+        total = 0
+        for num in data:
+            total += num
+        return total / len(data)
+
+    def calculate_median(self, data):
+        if not data:
+            return 0
+        sorted_data = sorted(data)
+        mid = len(sorted_data) // 2
+        if len(sorted_data) % 2 == 0:
+            return (sorted_data[mid] + sorted_data[mid - 1]) / 2
+        else:
+            return sorted_data[mid]
+
+    def calculate_mode(self, data):
+        if not data:
+            return 0
         count_dict = {}
         for num in data:
             if num in count_dict:
@@ -42,220 +71,193 @@ class Metricstics:
         else:
             return mode_values
 
-class MetricsticsFrame(ttk.Frame):
-    def __init__(self, container):
-        super().__init__(container)
-        # field options
-        options = {'padx': 2, 'pady': 2}
+    def calculate_mad(self, data):
+        if not data:
+            return 0
+        mean = self.calculate_mean(data)
+        total = 0
+        for num in data:
+            total += self.absolute(num - mean)
+        return total / len(data)
 
-        # range of data label
-        self.range_of_data_label = ttk.Label(self, text='Range of data')
+    def calculate_standard_deviation(self, data):
+        if not data:
+            return 0
+        mean = self.calculate_mean(data)
+        variance = 0
+        for num in data:
+            variance += (num - mean) ** 2
+        return math.sqrt(variance / (len(data) - 1))
+
+    def absolute(self, num):
+        if num < 0:
+            return -num
+        return num
+
+# MetricsticsFrame class represents the main UI for the application.
+class MetricsticsFrame(ttk.Frame):
+    def __init__(self, container, data_generator, metrics_calculator):
+        super().__init__(container)
+        self.data_generator = data_generator
+        self.metrics_calculator = metrics_calculator
+
+        # UI setup
+        options = {'padx': 5, 'pady': 5}
+
+        # Left frame for data generation
+        left_frame = ttk.Frame(self)
+        left_frame.grid(row=0, column=0, sticky=tk.NSEW)
+
+        self.range_of_data_label = ttk.Label(left_frame, text='Range of data')
         self.range_of_data_label.grid(column=0, row=0, sticky=tk.W, **options)
 
-        # data range
         self.data_range = tk.StringVar()
-        self.data_range_entry = ttk.Entry(self, textvariable=self.data_range)
+        self.data_range_entry = ttk.Entry(left_frame, textvariable=self.data_range)
         self.data_range_entry.grid(column=1, row=0, **options)
         self.data_range_entry.focus()
 
-        # button Generate Data
-        self.generate_button = ttk.Button(self, text='Generate Data')
-        self.generate_button['command'] = self.generate
-        self.generate_button.grid(column=2, row=0, sticky=tk.W, **options)
+        self.generate_button = ttk.Button(left_frame, text='Generate Data')
+        self.generate_button['command'] = self.generate_data
+        self.generate_button.grid(column=2, row=0, **options)
 
-        # result label
-        self.text_widget = tk.Text(self, height=15, width=80)
-        self.text_widget.grid(row=2, columnspan=50, **options)
+        self.data_text = scrolledtext.ScrolledText(left_frame, wrap=tk.WORD, width=30, height=10)
+        self.data_text.grid(row=1, column=0, columnspan=3, rowspan=3, **options)
 
-        # button Min
-        self.min_button = ttk.Button(self, text='Min')
-        self.min_button['command'] = self.min
-        self.min_button.grid(column=0, row=3, sticky=tk.SW, **options)
+        # Right frame for calculations and results
+        right_frame = ttk.Frame(self)
+        right_frame.grid(row=0, column=1, sticky=tk.NSEW)
 
-        # button Max
-        self.max_button = ttk.Button(self, text='Max')
-        self.max_button['command'] = self.max
-        self.max_button.grid(column=1, row=3, sticky=tk.SW, **options)
+        button_frame = ttk.Frame(right_frame)
+        button_frame.grid(row=0, column=0, sticky=tk.NSEW)
 
-        # button Median
-        self.median_button = ttk.Button(self, text='Med')
-        self.median_button['command'] = self.median
-        self.median_button.grid(column=2, row=3, sticky=tk.SW, **options)
+        self.mode_button = ttk.Button(button_frame, text='Mode')
+        self.mode_button.grid(column=0, row=0, sticky=tk.W, **options)
 
-        # button μ
-        self.median_button = ttk.Button(self, text='μ')
-        self.median_button['command'] = self.arithmeticMean
-        self.median_button.grid(column=3, row=3, sticky=tk.SW, **options)
+        self.mean_button = ttk.Button(button_frame, text='Mean (μ)')
+        self.mean_button.grid(column=0, row=1, sticky=tk.W, **options)
 
-        # button MAD
-        self.mad_button = ttk.Button(self, text='MAD')
-        self.mad_button['command'] = self.meanAbsoluteDeviation
-        self.mad_button.grid(column=4, row=3, sticky=tk.SW, **options)
+        self.median_button = ttk.Button(button_frame, text='Median')
+        self.median_button.grid(column=0, row=2, sticky=tk.W, **options)
 
-        # button σ
-        self.standard_deviation_button = ttk.Button(self, text='σ')
-        self.standard_deviation_button['command'] = self.standardDeviation
-        self.standard_deviation_button.grid(column=5, row=3, sticky=tk.SW, **options)
+        self.min_button = ttk.Button(button_frame, text='Min')
+        self.min_button.grid(column=0, row=3, sticky=tk.W, **options)
 
-        # button Mode
-        self.mode_button = ttk.Button(self, text='Mode')
-        self.mode_button['command'] = self.calculateMode
-        self.mode_button.grid(column=6, row=3, sticky=tk.SW, **options)
+        self.max_button = ttk.Button(button_frame, text='Max')
+        self.max_button.grid(column=0, row=4, sticky=tk.W, **options)
 
-        # Min data label
-        self.min_data_label = ttk.Label(self, text='')
-        self.min_data_label.grid(column=0, row=4, sticky=tk.W, **options)
+        self.mad_button = ttk.Button(button_frame, text='Mean Absolute Deviation (MAD)')
+        self.mad_button.grid(column=0, row=5, sticky=tk.W, **options)
 
-        # Max data label
-        self.max_data_label = ttk.Label(self, text='')
-        self.max_data_label.grid(column=1, row=4, sticky=tk.W, **options)
+        self.std_dev_button = ttk.Button(button_frame, text='Standard Deviation (σ)')
+        self.std_dev_button.grid(column=0, row=6, sticky=tk.W, **options)
 
-        # Median data label
-        self.median_data_label = ttk.Label(self, text='')
-        self.median_data_label.grid(column=2, row=4, sticky=tk.W, **options)
+        self.result_text = scrolledtext.ScrolledText(right_frame, wrap=tk.WORD, width=30, height=3)  # Smaller text box
+        self.result_text.grid(row=1, column=0, columnspan=1, rowspan=3, **options)
 
-        # ArithmeticMean data label
-        self.arithmeticMean_data_label = ttk.Label(self, text='')
-        self.arithmeticMean_data_label.grid(column=3, row=4, sticky=tk.W, **options)
+        # Set up button event handlers
+        self.mode_button['command'] = self.calculate_mode
+        self.mean_button['command'] = self.calculate_mean
+        self.median_button['command'] = self.calculate_median
+        self.min_button['command'] = self.calculate_min
+        self.max_button['command'] = self.calculate_max
+        self.mad_button['command'] = self.calculate_mad
+        self.std_dev_button['command'] = self.calculate_standard_deviation
 
-        # meanAbsoluteDeviation data label
-        self.meanAbsoluteDeviation_data_label = ttk.Label(self, text='')
-        self.meanAbsoluteDeviation_data_label.grid(column=4, row=4, sticky=tk.W, **options)
-
-        # standardDeviation data label
-        self.standardDeviation_data_label = ttk.Label(self, text='')
-        self.standardDeviation_data_label.grid(column=5, row=4, sticky=tk.W, **options)
-
-        # Mode data label
-        self.mode_data_label = ttk.Label(self, text='')
-        self.mode_data_label.grid(column=6, row=4, sticky=tk.W, **options)
-
-        # add padding to the frame and show it
         self.grid(padx=20, pady=20, sticky=tk.NSEW)
 
-    def standardDeviation(self):
-        try:
-            data = Metricstics.generated_data
-            if data:
-                mean = int(self.arithmeticMean())
-                variance = 0
-                for num in data:
-                    variance += (num - mean) ** 2
-                result = math.sqrt(variance / (len(data) - 1))
-                new_text = 'σ: ' + str(round(result, 3))
-                self.standardDeviation_data_label.config(text=new_text)
+    def calculate_mode(self):
+        data = self.data_generator.generate_data(int(self.data_range.get()))
+        if isinstance(data, list):
+            result = self.metrics_calculator.calculate_mode(data)
+            self.update_result_text(f"Mode: {result}")
+        else:
+            self.update_result_text(data)
 
-        except ValueError as error:
-            showerror(title='Error', message=error)
-    def meanAbsoluteDeviation(self):
-        try:
-            data = Metricstics.generated_data
-            if data:
-                mean = int(self.arithmeticMean())
-                total = 0
-                for num in data:
-                    total += Metricstics.abs(num - mean)
-                result = str(total / len(data))
-                new_text = 'MAD: ' + str(result)
-                self.meanAbsoluteDeviation_data_label.config(text=new_text)
+    def calculate_mean(self):
+        data = self.data_generator.generate_data(int(self.data_range.get()))
+        if isinstance(data, list):
+            result = self.metrics_calculator.calculate_mean(data)
+            self.update_result_text(f"Mean (μ): {result}")
+        else:
+            self.update_result_text(data)
 
-        except ValueError as error:
-            showerror(title='Error', message=error)
+    def calculate_median(self):
+        data = self.data_generator.generate_data(int(self.data_range.get()))
+        if isinstance(data, list):
+            result = self.metrics_calculator.calculate_median(data)
+            self.update_result_text(f"Median: {result}")
+        else:
+            self.update_result_text(data)
+                
+    def calculate_median(self):
+        data = self.data_generator.generate_data(int(self.data_range.get()))
+        if isinstance(data, list):
+            result = self.metrics_calculator.calculate_median(data)
+            self.update_result_text(f"Median: {result}")
+        else:
+            self.update_result_text(data)
 
-    def arithmeticMean(self):
-        try:
-            data = Metricstics.generated_data
-            total = 0
-            if data:
-                for num in data:
-                    total += num
-                result = str(total / len(data))
-                new_text = 'μ: ' + str(result)
-                self.arithmeticMean_data_label.config(text=new_text)
+    def calculate_min(self):
+        data = self.data_generator.generate_data(int(self.data_range.get()))
+        if isinstance(data, list):
+            result = self.metrics_calculator.calculate_min(data)
+            self.update_result_text(f"Min: {result}")
+        else:
+            self.update_result_text(data)
 
-                return total / len(data)
+    def calculate_max(self):
+        data = self.data_generator.generate_data(int(self.data_range.get()))
+        if isinstance(data, list):
+            result = self.metrics_calculator.calculate_max(data)
+            self.update_result_text(f"Max: {result}")
+        else:
+            self.update_result_text(data)
 
-        except ValueError as error:
-            showerror(title='Error', message=error)
-    def median(self):
-        try:
-            data = Metricstics.generated_data
-            if data:
-                mid = len(data)//2
-                if len(data) % 2 == 0:
-                    result = str((data[mid] + data[mid-1])/2)
-                else:
-                    result = str(data[mid])
-                new_text = 'Median: ' + str(result)
-                self.median_data_label.config(text=new_text)
+    def calculate_mad(self):
+        data = self.data_generator.generate_data(int(self.data_range.get()))
+        if isinstance(data, list):
+            result = self.metrics_calculator.calculate_mad(data)
+            self.update_result_text(f"Mean Absolute Deviation (MAD): {result}")
+        else:
+            self.update_result_text(data)
 
-        except ValueError as error:
-            showerror(title='Error', message=error)
+    def calculate_standard_deviation(self):
+        data = self.data_generator.generate_data(int(self.data_range.get()))
+        if isinstance(data, list):
+            result = self.metrics_calculator.calculate_standard_deviation(data)
+            self.update_result_text(f"Standard Deviation (σ): {result}")
+        else:
+            self.update_result_text(data)
 
-    def min(self):
-        try:
-            data = Metricstics.generated_data
-            if data:  # Ensure c is not an empty list
-                min_value = float('inf')
-                for value in data:
-                    if value < min_value:
-                        min_value = value
-                new_text = 'Min: ' + str(min_value)
-                self.min_data_label.config(text=new_text)
+    def generate_data(self):
+        data = self.data_generator.generate_data(int(self.data_range.get()))
+        if isinstance(data, list):
+            self.update_data_text("Generated Data:\n" + ", ".join(map(str, data))
+        )
+        else:
+            self.update_data_text(data)
 
-        except ValueError as error:
-            showerror(title='Error', message=error)
+    def update_data_text(self, text):
+        self.data_text.delete(1.0, tk.END)
+        self.data_text.insert(tk.END, text)
 
-    def max(self):
-        try:
-            data = Metricstics.generated_data
-            if data:  # Ensure c is not an empty list
-                max_value = float('-inf')
-                for value in data:
-                    if value > max_value:
-                        max_value = value
-                new_text = 'Max: ' + str(max_value)
-                self.max_data_label.config(text=new_text)
+    def update_result_text(self, text):
+        self.result_text.delete(1.0, tk.END)
+        self.result_text.insert(tk.END, text)
 
-        except ValueError as error:
-            showerror(title='Error', message=error)
-
-    def calculateMode(self):
-        try:
-            data = Metricstics.generated_data
-            if data:
-                result = Metricstics.mode(data)
-                if isinstance(result, list):
-                    result = ", ".join(map(str, result))
-                self.mode_data_label.config(text=result)
-        except ValueError as error:
-            showerror(title='Error', message=error)
-
-    def generate(self):
-        """  Handle button Generate event
-        """
-        try:
-            f = int(self.data_range.get())
-            data = Metricstics.generate_data(f)
-
-            self.text_widget.delete(1.0, tk.END)
-            if isinstance(data, list):
-                self.text_widget.insert(tk.END, ", ".join(map(str, data)))
-            else:
-                self.text_widget.insert(tk.END, data)
-
-        except ValueError as error:
-            showerror(title='Error', message=error)
-
+# App class represents the main application window.
 class App(tk.Tk):
-    def __init__(self):
+    def __init__(self, data_generator, metrics_calculator):
         super().__init__()
-
         self.title('METRICSTICS')
-        self.geometry('800x350')
+        self.geometry('700x350')  # Adjusted window width
         self.resizable(False, False)
+        self.data_generator = data_generator
+        self.metrics_calculator = metrics_calculator
 
 if __name__ == "__main__":
-    app = App()
-    MetricsticsFrame(app)
+    data_generator = DataGenerator()
+    metrics_calculator = Metricstics()
+    app = App(data_generator, metrics_calculator)
+    MetricsticsFrame(app, data_generator, metrics_calculator)
     app.mainloop()
